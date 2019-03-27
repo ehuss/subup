d398c1d2b7663af5e46db15099189065e155fe52
Eric Huss <eric@huss.org>
2019-03-27 11:11:54 -0700

    Add message-only tool.

---
+++ src/bin/subup-msg.rs
@@ -0,0 +1,71 @@
+#[macro_use]
+extern crate clap;
+extern crate failure;
+extern crate subup;
+
+use clap::{App, Arg};
+use failure::Error;
+
+use subup::cli::Cli;
+use subup::log;
+
+fn get_hash(cli: &Cli, tree: &str, path: &str) -> Result<String, Error> {
+    let output = cli
+        .git(&format!("ls-tree {} {}", tree, path))
+        .capture_stdout("Failed to ls-tree")?;
+    Ok(output
+        .split_whitespace()
+        .skip(2)
+        .next()
+        .unwrap()
+        .to_string())
+}
+
+fn doit(cli: &Cli) -> Result<(), Error> {
+    cli.status("Generating .SUBUP_COMMIT_MSG")?;
+    // (path, start_hash, end_hash)
+    let submodules = cli
+        .matches
+        .values_of("submodules")
+        .unwrap()
+        .map(|submodule| {
+            let first = get_hash(cli, cli.matches.value_of("branch").unwrap(), submodule)?;
+            // TODO: Support uncommitted changes.
+            // git submodule status --cached src/tools/cargo
+            let current = get_hash(cli, "HEAD", submodule)?;
+            Ok((submodule, first, current))
+        })
+        .collect::<Result<Vec<_>, Error>>()?;
+    log::generate_commit(cli, &submodules, "master")?;
+    cli.status("Complete")?;
+    Ok(())
+}
+
+fn main() {
+    let matches = App::new("subup-msg")
+        .version(crate_version!())
+        .about("Generate commit message")
+        .setting(clap::AppSettings::ColoredHelp)
+        .arg(
+            Arg::with_name("submodules")
+                .help("Submodules to examine")
+                .multiple(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("verbose")
+                .long("verbose")
+                .short("v")
+                .help("Verbose output"),
+        )
+        .arg(
+            Arg::with_name("branch")
+                .long("branch")
+                .help("Parent branch")
+                .default_value("master"),
+        )
+        .get_matches();
+
+    let cli = Cli::new(matches);
+    cli.doit(doit);
+}
